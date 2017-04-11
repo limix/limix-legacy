0434cdafc66c0ba3ad0adc321442a2ae35de943a
Danilo Horta <danilo.horta@gmail.com>
2017-04-11 14:41:17 +0100

    release without mac workaround

--- setup.py
+++ setup.py
@@ -177,7 +177,7 @@ def setup_package(reswig, compatible):
 
     metadata = dict(
         name="limix-legacy",
-        version="0.8.8",
+        version="0.8.9",
         description="A flexible and fast mixed model toolbox.",
         long_description=long_description,
         keywords='linear mixed models, GWAS, QTL, ' +
