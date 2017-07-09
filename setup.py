a1bf280d0115de058acab8b3b790b712246a8c4b
Danilo Horta <danilo.horta@gmail.com>
2017-07-09 21:50:14 +0100

    bump

--- setup.py
+++ setup.py
@@ -93,8 +93,8 @@ def core_extension(reswig):
 
     def swig_opts():
         return [
-            '-c++', '-outdir', join('limix_legacy', 'deprecated'),
-            '-I' + join('src')
+            '-c++', '-outdir',
+            join('limix_legacy', 'deprecated'), '-I' + join('src')
         ]
 
     def nlopt_files():
@@ -170,7 +170,7 @@ def setup_package(reswig, compatible):
 
     metadata = dict(
         name="limix-legacy",
-        version="0.8.12",
+        version="0.8.13",
         description="A flexible and fast mixed model toolbox.",
         long_description=long_description,
         keywords='linear mixed models, GWAS, QTL, ' +
